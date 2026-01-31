022fe62a0ea5badce393823e2c47fff4beb46d84
imkrad <164296287+imkrad@users.noreply.github.com>
2026-01-31 20:16:55 +0800

    aw

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title inertia>TasweerHub</title>
-        <meta name="description" content="Laboratory Information Management System">
-        <meta name="keywords" content="DOST, LIMS, ONELAB">
+        <meta name="description" content="Photography">
+        <meta name="keywords" content="Krad">
         <meta name="author" content="Krad">
-        <meta property="og:title" content="LIMS - Laboratory Information Management System">
-        <meta property="og:description" content="Laboratory Management System">
+        <meta property="og:title" content="TasweerHub Photography">
+        <meta property="og:description" content="Photography">
         <meta property="og:image" content="URL to the template's logo or featured image">
         <meta property="og:url" content="URL to the template's webpage">
         <meta name="twitter:card" content="summary_large_image">
